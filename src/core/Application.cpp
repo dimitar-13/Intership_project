a9efe97b44c91b26071312198d91c840e8cefae5
Dimitar Najdenov <dimitarnaidenov5@gmail.com>
2025-05-02 14:04:51 +0300

    Implemented duplicate function.

--- src/core/Application.cpp
+++ src/core/Application.cpp
@@ -146,19 +146,26 @@ void Application::RemoveFigure()
 
 void Application::DuplicateAndAppendToEnd()
 {
-    size_t selected_figure_index = 0;
+    size_t selected_figure_number = 0;
 
     ListFigures();
     std::cout << "Enter the of figure you want to copy(1,2,3 etc.)" << '\n';
 
-    std::cin >> selected_figure_index;
+    std::cin >> selected_figure_number;
 
-    if (selected_figure_index > m_user_figure_list.size())
+    if (selected_figure_number > m_user_figure_list.size())
     {
         std::cout << "Figure does not exist" << '\n';
         return;
     }
-    Prototype* prototype_cast = dynamic_cast<Prototype*>(m_user_figure_list[selected_figure_index]);
+
+    Prototype* prototype_cast = dynamic_cast<Prototype*>(m_user_figure_list[selected_figure_number - 1]);
+
+    if (prototype_cast == nullptr)
+    {
+        std::cout << "Invalid figure type" << '\n';
+        return;
+    }
 
     m_user_figure_list.push_back(dynamic_cast<Shape*>(prototype_cast->Clone()));
 }
